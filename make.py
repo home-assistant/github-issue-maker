7b83f80a4b3e344a2aedc70ad49198cc4607d2c9
Martin Hjelmare <marhje52@gmail.com>
2020-09-30 11:37:15 +0200

    Add CLI (#1)

--- make.py
+++ make.py
@@ -1,24 +1,36 @@
+#!/usr/bin/env python3
+"""Make issues on github.com."""
+from dataclasses import dataclass
+from functools import partial
 from pathlib import Path
 from pprint import pprint
-from datetime import datetime
-import sys
-import requests
-import json
 
-# Authentication for user filing issue (must have read/write access to
-# repository to add issue to)
-USERNAME = "balloob"
-TOKEN = Path(".token").read_text().strip()
+import click
+import requests
 
 # The repository to add this issue to
 REPO_OWNER = "home-assistant"
 REPO_NAME = "core"
 
+SETTINGS = dict(help_option_names=["-h", "--help"])
 
-def make_github_issue(title, body=None, assignee=None, milestone=None, labels=None):
+
+@dataclass
+class Auth:
+    """Represent the github authentication details."""
+
+    repo_name: str
+    repo_owner: str
+    token: str
+    username: str
+
+
+def make_github_issue(
+    auth, title, body=None, assignee=None, milestone=None, labels=None
+):
     """Create an issue on github.com using the given parameters."""
     # Our url to create issues via POST
-    url = "https://api.github.com/repos/%s/%s/issues" % (REPO_OWNER, REPO_NAME)
+    url = f"https://api.github.com/repos/{auth.repo_owner}/{auth.repo_name}/issues"
 
     # Create our issue
     issue = {
@@ -31,28 +43,30 @@ def make_github_issue(title, body=None, assignee=None, milestone=None, labels=No
 
     # Add the issue to our repository
     response = requests.post(
-        url, json=issue, headers={"Authorization": "token %s" % TOKEN}
+        url, json=issue, headers={"Authorization": f"token {auth.token}"}
     )
 
     print(response.status_code)
     pprint(response.json())
 
     if response.status_code == 201:
-        print('Successfully created Issue "%s"' % title)
+        print(f'Successfully created Issue "{title}"')
     else:
-        print('Could not create Issue "%s"' % title)
+        print(f'Could not create Issue "{title}"')
         print("Response:", response.content)
 
 
-def make_github_issue_no_notify(title, body, labels):
-    """Does not fire webhooks or create notifications."""
+def make_github_issue_no_notify(auth, title, body, labels):
+    """Create issues while not firing webhooks or creating notifications."""
     # Create an issue on github.com using the given parameters
     # Url to create issues via POST
-    url = "https://api.github.com/repos/%s/%s/import/issues" % (REPO_OWNER, REPO_NAME)
+    url = (
+        f"https://api.github.com/repos/{auth.repo_owner}/{auth.repo_name}/import/issues"
+    )
 
     # Headers
     headers = {
-        "Authorization": "token %s" % TOKEN,
+        "Authorization": f"token {auth.token}",
         "Accept": "application/vnd.github.golden-comet-preview+json",
     }
 
@@ -78,52 +92,114 @@ def make_github_issue_no_notify(title, body, labels):
     pprint(response.json())
 
     if response.status_code == 202:
-        print('Successfully created Issue "%s"' % title)
+        print(f'Successfully created Issue "{title}"')
     else:
-        print('Could not create Issue "%s"' % title)
+        print(f'Could not create Issue "{title}"')
         print("Response:", response.content)
 
 
-template = Path("./issues/test_uncaught.markdown").read_text()
+@click.group(
+    options_metavar="", subcommand_metavar="<command>", context_settings=SETTINGS
+)
+def cli():
+    """Batch create github.com issues."""
 
-for domain in [
-    # "abode",
-    # "cast",
-    # "config",
-    "deconz",
-    "default_config",
-    "demo",
-    "discovery",
-    "dsmr",
-    "dynalite",
-    "dyson",
-    "gdacs",
-    "geonetnz_quakes",
-    "homematicip_cloud",
-    "hue",
-    "ios",
-    "local_file",
-    "meteo_france",
-    "mikrotik",
-    "mqtt",
-    "plex",
-    "qwikswitch",
-    "rflink",
-    "samsungtv",
-    "tplink",
-    "tradfri",
-    "unifi_direct",
-    "upnp",
-    "vera",
-    "wunderground",
-    "yr",
-    "zha",
-    "zwave",
-]:
-    title = f"Fix {domain} tests that have uncaught exceptions"
-    body = template.replace("{{ DOMAIN }}", domain)
-    labels = [f"integration: {domain}", "to do"]
-    # print(title)
-    # print(body)
-    # print(labels)
-    make_github_issue(title=title, body=body, labels=labels)
+
+@click.command(name="issue", options_metavar="<options>")
+@click.option(
+    "-s", "--silent", is_flag=True, help="Make an issue without notifications."
+)
+@click.option(
+    "-t",
+    "--token",
+    prompt=True,
+    hide_input=True,
+    default="",
+    help="Set the auth token.",
+)
+@click.option(
+    "-R",
+    "--repo",
+    default=REPO_NAME,
+    show_default=True,
+    help="Set the target repo.",
+)
+@click.option(
+    "-u",
+    "--username",
+    required=True,
+    help="Set the username.",
+)
+@click.option(
+    "-O",
+    "--owner",
+    default=REPO_OWNER,
+    show_default=True,
+    help="Set the repository owner.",
+)
+@click.option(
+    "-T",
+    "--title",
+    required=True,
+    help="Set the issue title.",
+)
+@click.option(
+    "-b",
+    "--body",
+    required=True,
+    type=click.Path(exists=True, dir_okay=False),
+    help="Set path to a text file with issue body.",
+)
+@click.option(
+    "-a",
+    "--assignee",
+    help="Set the issue assignee.",
+)
+@click.option(
+    "-m",
+    "--milestone",
+    help="Set the issue milestone.",
+)
+@click.option("-l", "--labels", multiple=True, help="Set the issue labels.")
+def create_issue(silent, owner, repo, token, username, title, body, labels, **kwargs):
+    """Create issue on github.com."""
+    if not token:
+        try:
+            token = Path(".token").read_text().strip()
+        except FileNotFoundError:
+            print(
+                "Missing '.token' file. "
+                "Token must be provided by promt or in a '.token' file"
+            )
+            return
+
+    body = Path(body).read_text()
+
+    # Authentication for user filing issue (must have read/write access to
+    # repository to add issue to)
+    auth = Auth(repo_name=repo, repo_owner=owner, username=username, token=token)
+
+    issue_func = partial(make_github_issue, auth, **kwargs)
+
+    if silent:
+        # the import API doesn't accept optional values
+        issue_func = partial(make_github_issue_no_notify, auth)
+
+    domain_names = Path("domains.txt").read_text().strip().splitlines()
+    domain_title = None
+    domain_body = None
+    domain_labels = None
+    for domain in domain_names:
+        domain_title = title.replace("{{ DOMAIN }}", domain)
+        domain_body = body.replace("{{ DOMAIN }}", domain)
+        if labels:
+            domain_labels = labels + (f"integration: {domain}",)
+
+        issue_func(title=domain_title, body=domain_body, labels=domain_labels)
+
+
+cli.add_command(create_issue)
+
+
+if __name__ == "__main__":
+    cli()
